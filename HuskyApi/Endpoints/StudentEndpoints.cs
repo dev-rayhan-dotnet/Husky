865ae0ca7553816e7972bc62eea7d59ead3c8552
rayhan <rayhan@ibos.io>
2024-08-21 18:16:20 +0600

    init

---
+++ HuskyApi/Endpoints/StudentEndpoints.cs
@@ -0,0 +1,32 @@
+namespace HuskyApi.Endpoints;
+
+public static class StudentEndpoints
+{
+    public static void MapStudentEndpoints(this WebApplication app)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/student", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        (
+                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            Random.Shared.Next(-20, 55),
+                            summaries[Random.Shared.Next(summaries.Length)]
+                        ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetAllStudent")
+            .WithOpenApi();
+    }
+
+    private record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
